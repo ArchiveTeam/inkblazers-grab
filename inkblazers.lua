c28aab43fb658f1a89306df4d5a4ce5f651ec372
Arkiver2 <Arkiver@hotmail.com>
2015-01-18 17:50:40 +0100

    inkblazers.lua: add comments url

--- inkblazers.lua
+++ inkblazers.lua
@@ -67,7 +67,8 @@ wget.callbacks.get_urls = function(file, url, is_css, iri)
       check(datalink)
       local datapicture = string.match(html, 'data-picture="[^"]+"')
       check(datapicture)
-      local dataurl = "http://www.inkblazers.com"..string.match(html, 'data-url="[^"]+"')
+      local dataurl1 = string.match(html, 'data-url="[^"]+"')
+      local dataurl = "http://www.inkblazers.com"..dataurl1
       check(dataurl)
     end
   end
