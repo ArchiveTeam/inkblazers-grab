1bd80787bd03fffdf205868cef7ee1404a874d58
Arkiver2 <Arkiver@hotmail.com>
2015-01-31 17:57:42 +0100

    inkblazers.lua: skip url after 5-10 tries

--- inkblazers.lua
+++ inkblazers.lua
@@ -237,7 +237,7 @@ wget.callbacks.httploop_result = function(url, err, http_stat)
     if tries >= 5 then
       io.stdout:write("\nI give up...\n")
       io.stdout:flush()
-      return wget.actions.ABORT
+      return wget.actions.EXIT
     else
       return wget.actions.CONTINUE
     end
@@ -252,7 +252,7 @@ wget.callbacks.httploop_result = function(url, err, http_stat)
     if tries >= 10 then
       io.stdout:write("\nI give up...\n")
       io.stdout:flush()
-      return wget.actions.ABORT
+      return wget.actions.EXIT
     else
       return wget.actions.CONTINUE
     end
